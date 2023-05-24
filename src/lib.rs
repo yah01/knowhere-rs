ad9dcea5df1e4104f350102f8067866587682462
yah01 <yang.cen@zilliz.com>
2023-05-24 17:42:01 +0800

    Compile knowhere
    Signed-off-by: yah01 <yang.cen@zilliz.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+mod knowhere;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use super::knowhere::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn test_knowhere() {}
 }
